049a8b45d8e2d1f7caf4ee5b713047e3610a7a20
jlz <jlz@qq.com>
2020-04-28 14:14:48 +0800

    gitupdate

--- movieutils/config.py
+++ movieutils/config.py
@@ -15,5 +15,5 @@ fontfile = (fonts_dir / 'NotoSansSC-Medium.otf').absolute()
 
 # prefix and segments
 video_prefix_path = pathlib.Path('./aisiji/cn_0.mp4')
-video_segment_path = pathlib.Path('./aisiji/9499.mp4')
+video_segment_path = pathlib.Path('./aisiji/cn_1.mp4')
 
