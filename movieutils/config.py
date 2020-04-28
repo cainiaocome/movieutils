2117c6dfa919f58e87014407cea5d6c98690229e
jlz <jlz@qq.com>
2020-04-28 11:28:13 +0800

    gitupdate

---
+++ movieutils/config.py
@@ -0,0 +1,19 @@
+
+import pathlib
+from pprint import pprint
+import tmdbsimple as tmdb
+    
+
+# videos dir
+videos_dir = pathlib.Path('./videos/')
+
+
+# font files
+fonts_dir = pathlib.Path('fonts')
+fontfile = (fonts_dir / 'NotoSansSC-Medium.otf').absolute()
+
+
+# prefix and segments
+video_prefix_path = pathlib.Path('./aisiji/cn_0.mp4')
+video_segment_path = pathlib.Path('./aisiji/9499.mp4')
+
--- movieutils/init.py
+++ movieutils/init.py
@@ -2,6 +2,7 @@
 
 import shlex
 import subprocess
+from pprint import pprint
 def run(cmd):
     subprocess.run(cmd, shell=True)
 
@@ -32,28 +33,9 @@ wget https://fonts.google.com/download?family=Noto%20Sans%20SC -O noto.zip
 dtrx noto.zip --one here
 cp noto/* fonts/
 '''
-def do_init_env():
-    cmds = cmds.split()
-    for cmd in cmds:
-        print(cmd)
-        run(cmd)
 
-
-import pathlib
-from pprint import pprint
-import tmdbsimple as tmdb
-    
-
-# videos dir
-videos_dir = pathlib.Path('./videos/')
-
-
-# font files
-fonts_dir = pathlib.Path('fonts')
-fontfile = (fonts_dir / 'NotoSansSC-Medium.otf').absolute()
-
-
-# prefix and segments
-video_prefix_path = pathlib.Path('./aisiji/cn_0.mp4')
-video_segment_path = pathlib.Path('./aisiji/9499.mp4')
-
+cmds = cmds.split()
+pprint(cmds)
+for cmd in cmds:
+    print(cmd)
+    run(cmd)
