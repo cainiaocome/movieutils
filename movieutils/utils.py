a504261ae6727366239b107f7f0147b36620fe21
jlz <jlz@qq.com>
2020-04-29 15:17:00 +0800

    gitupdate

--- movieutils/utils.py
+++ movieutils/utils.py
@@ -1,8 +1,10 @@
 #/usr/bin/env python
 
+import pathlib
 import shutil
 import shlex
 import subprocess
+import time
 from datetime import datetime
 
 
@@ -12,19 +14,25 @@ def run(cmd):
 def rmdir(d):
     shutil.rmtree(d, ignore_errors=True) 
 
+ffsend_urls = []
 def ffsend(filepath):
-    import shlex
-    import subprocess
-    import time
     ffsend_basic_auth = 'cainiaocome@gmail.com:t/b+8nESha_#dG*'
     cmd = f'ffsend upload --no-interact --expiry-time 1d {filepath}'.split()
     while True:
         try:
             output = subprocess.check_output(cmd)
             url = output.decode('utf8').strip()
+            ffsend_urls.append(url)
             return url
         except:
             print(f'ffsend upload {filepath} failed')
             time.sleep(30)
+
+def ffsend_and_delete(filepath):
+    fp = str(filepath)
+    url = ffsend(filepath)
+    p = pathlib.Path(filepath)
+    p.unlink()
+    return url
 
 today = f'{datetime.now().date()}'
