57e9e73eb90230d44c05954eaff5ee97c3c9f1c8
root <root@raspberrypi>
2020-04-30 23:13:00 +0800

    save

--- movieutils/init.py
+++ movieutils/init.py
@@ -37,6 +37,8 @@ rm -rf fonts/*
 wget https://fonts.google.com/download?family=Noto%20Sans%20SC -O noto.zip
 dtrx noto.zip --one here
 cp noto/* fonts/
+
+!git clone https://github.com/cainiaocome/movieutils.git
 '''
 
 cmds = cmds.splitlines()
